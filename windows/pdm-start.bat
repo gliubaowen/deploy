463b1ff8c8b8b76ac20eb8de1a949e92a25c2864
wliub <wliub@DESKTOP-HTSJ8RC>
2019-04-17 09:57:14 +0800

    修改 启动pdm文件脚本

--- windows/pdm-start.bat
+++ windows/pdm-start.bat
@@ -6,7 +6,7 @@ echo 打开product.pdm文件开始
 
 set file="D:\01.我的资料\06.Project\liqun\doc\liqun-doc\04 系统设计\31 数据库设计\10 中台\商品\product.pdm"
 set exe="C:\Program Files (x86)\Sybase\PowerDesigner 16\PdShell16.exe"
-start %exe% %var%
+start %exe% %file%
 
 echo 打开product.pdm文件完成
 
